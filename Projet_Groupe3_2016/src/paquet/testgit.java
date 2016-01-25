8d313e91214a383398a6631d9788a9ea8601dae5
mmoren01 <mmoren01@MM302>
2016-01-25 11:57:19 +0100

    commit 3

--- Projet_Groupe3_2016/src/paquet/testgit.java
+++ Projet_Groupe3_2016/src/paquet/testgit.java
@@ -1,8 +1,12 @@
 package paquet;
 
 import java.io.*;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Color;
 
@@ -10,19 +14,23 @@ import java.awt.Color;
 public class testgit {	
 	public static void main(String[] args) {
 		// pour lancer une commande unix
-                String cmd;
-                cmd = "gedit";
+        //        String cmd;
+        //       cmd = "gedit";
         // lance la commande precedente
-		try {
+		/*try {
 			Runtime r = Runtime.getRuntime();
                         Process p = r.exec(cmd);
                         p.waitFor();//si l'application doit attendre a ce que ce process fini
 		}catch(Exception e) {
 			System.out.println("erreur d'execution " + cmd + e.toString());
-                }
-		
+                }*/
+		//ouvre une fenetre
 		JFrame cadre = new javax.swing.JFrame("Selection du git");
 		JPanel panneau = new JPanel();
+		JTextField git = new JTextField();
+		Container contentpane = cadre.getContentPane();
+		contentpane.add (git);
+		
 		panneau.setPreferredSize(new Dimension(350, 200));
 	
 		panneau.setBackground(Color.white);
@@ -31,5 +39,6 @@ public class testgit {
 		cadre.pack();
 		cadre.setVisible(true);
 		cadre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
     }
 }
