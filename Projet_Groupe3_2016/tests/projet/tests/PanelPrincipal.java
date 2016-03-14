8756f242108e9e49abdf4efa5023fe837ba5edcd
mmoren01 <mmoren01@MM302>
2016-03-14 11:10:05 +0100

    Interface- lundi 14 Mars 2016

--- Projet_Groupe3_2016/tests/projet/tests/PanelPrincipal.java
+++ Projet_Groupe3_2016/tests/projet/tests/PanelPrincipal.java
@@ -2,6 +2,10 @@ package projet.tests;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -9,9 +13,10 @@ import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+
+import java.awt.Dimension;
  
-public class PanelPrincipal extends JPanel {
+public class PanelPrincipal extends JPanel implements ActionListener {
  
 	private JButton blob = new JButton("blob");
 	private JButton tree = new JButton("tree");
@@ -19,6 +24,8 @@ public class PanelPrincipal extends JPanel {
 	private JFrame fenetre;
 	private JPanel panelNord = null;
 	private JPanel panelCentre = null;
+	private JPanel panelWest = null;
+	private JPanel panelEast = null;
  
 	public PanelPrincipal() {
  
@@ -31,44 +38,91 @@ public class PanelPrincipal extends JPanel {
  
 		java.awt.Container container = this.fenetre.getContentPane();
 		BorderLayout disposition = new BorderLayout();
- 
+		
 		container.setLayout(disposition);
-		container.setPreferredSize(new java.awt.Dimension(800, 600));
- 
-		fenetre.setSize(10, 100);
+		container.setPreferredSize(new java.awt.Dimension(1000, 1000));
+		/*
+		fenetre.setSize(100, 100);
 		fenetre.setLocationRelativeTo(getParent());
- 
-		container.add(getPanelNord(), BorderLayout.NORTH);
+		*/
+
+		/*
+		
 		container.add(getPanelCentre(), BorderLayout.CENTER);
- 
+
+		*/
+		container.add(getPanelCentre(), BorderLayout.CENTER);
+		container.add(getPanelWest(), BorderLayout.WEST);
+		container.add(getPanelEast(), BorderLayout.EAST);
+		container.add(getPanelNord(), BorderLayout.NORTH);
+		
 		this.fenetre.pack();
 		this.fenetre.setVisible(true);
-		
+		this.fenetre.setPreferredSize(new Dimension(1000, 1000));
+		/*
 		
 		container.add(blob);
 		container.add(tree);
 		container.add(commit);
 		blob.addActionListener((ActionListener) this);
 		tree.addActionListener((ActionListener) this);
-		commit.addActionListener((ActionListener) this);
+		commit.addActionListener((ActionListener) this);*/
+	}
+ 
+	private Component getPanelEast() {
+		if(panelEast == null) {
+			panelEast = new JPanel();
+			panelEast.setBackground(Color.RED);
+			panelEast.setPreferredSize(new Dimension(100, 100));
+		}
+		return panelEast;
+	}
+
+	private Component getPanelWest() {
+		if(panelWest == null) {
+			panelWest = new JPanel();
+			panelWest.setBackground(Color.BLUE);
+			panelWest.setPreferredSize(new Dimension(100, 200));
+		}
+		return panelWest;
+		
+	}
+
+	public JPanel getPanelCentre() {
+		if(panelCentre == null) {
+			panelCentre = new JPanel();
+			panelCentre.setBackground(Color.ORANGE);
+		}		
+		return panelCentre;
 	}
  
 	public JPanel getPanelNord() {
+		BorderLayout disposition = new BorderLayout();
 		if(panelNord == null) {
-			panelNord = new JPanel();
-			panelNord.setBackground(Color.ORANGE);
-		}		
-		return panelNord;
-	}
- 
-	public JPanel getPanelCentre() {
-		if(panelCentre == null) {
-			panelCentre =  new PanelSecondaire().getPanelSecondaire();
+		
+			
+			//java.awt.Container container = panelCentre.get;
+			FlowLayout disposition1 = new FlowLayout();
+			panelNord = new JPanel(disposition1);
+			panelNord.setPreferredSize(new Dimension(500, 60));
+			
+			blob.setPreferredSize(new Dimension(100, 50));
+			tree.setPreferredSize(new Dimension(100, 50));
+			commit.setPreferredSize(new Dimension(100, 50));
+			panelNord.add(blob);
+			panelNord.add(tree);
+			panelNord.add(commit);
 		}
-		return panelCentre;
+		return panelNord;
 	}
  
 	public static void main(String args[]) {
 		new PanelPrincipal();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getSource().toString());
 	}
 }
--- Projet_Groupe3_2016/tests/projet/tests/PanelSecondaire.java
+++ Projet_Groupe3_2016/tests/projet/tests/PanelSecondaire.java
@@ -1,24 +1,25 @@
 package projet.tests;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.JPanel;
  
-public class PanelSecondaire extends JPanel {
+public class PanelSecondaire extends PanelPrincipal {
 
 	private JPanel panelSecondaire = null;
  
 	public PanelSecondaire() {
  
 		//getPanelSecondaire();
- 
+		BorderLayout disposition = new BorderLayout();
 	}
  
 	public JPanel getPanelSecondaire() {
 		if(panelSecondaire == null) {
 			panelSecondaire = new JPanel();
 			panelSecondaire.setBackground(Color.blue);
-			panelSecondaire.setSize(100,100);			
+			panelSecondaire.setSize(200,200);			
 		}
 		return panelSecondaire;
 	}
