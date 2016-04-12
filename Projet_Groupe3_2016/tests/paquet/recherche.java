2b9698a4c2a029c8b3b5c886d29c7855537411a1
Alexandre Badez <alexandre.badez@gmail.com>
2016-04-12 22:42:45 +0200

    Ajout  de remarques.

--- Projet_Groupe3_2016/tests/paquet/recherche.java
+++ Projet_Groupe3_2016/tests/paquet/recherche.java
@@ -1,22 +1,21 @@
 package paquet;
 import java.io.File;
-
 import javax.swing.JOptionPane;
  
-public class recherche {
-	public recherche() {
+public class Recherche {
+	public Recherche() {
 		super();
 	}
-	
-	public  void findFiles(String directoryPath) {
+//fonction qui affiche le nombre de sous dossier contenue dans le lien donné
+	public  void afficheConsole(String directoryPath) {
 		File directory = new File(directoryPath);
 		if(!directory.exists()){
 			System.out.println("Le fichier/répertoire '"+directoryPath+"' n'existe pas");
 		}else if(!directory.isDirectory()){
-			System.out.println("Le chemin '"+directoryPath+"' correspond à un fichier et non à un répertoire");
+			System.out.println("Le chemin '"+directoryPath+"' correspond à un fichier et nom à un répertoire");
 		}else{
 			File[] subfiles = directory.listFiles();
-			String message = "Le répertoire '"+directoryPath+"' contient "+ subfiles.length+" fichier"+(subfiles.length>1?"s":"");
+			String message = "Le répertoire '"+directoryPath+"' contient "+ subfiles.length+" dossier"+(subfiles.length>1?"s":"");
 			System.out.println(message);
 			for(int i=0 ; i<subfiles.length; i++){
 				System.out.println(subfiles[i].getName());
@@ -24,11 +23,6 @@ public class recherche {
 		}
 	}
 
-	public static void main(String[] args) {
-		JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane();
-	    String nom = JOptionPane.showInputDialog(null, "Veuillez entrer le chemin pour acceder au dossier !", JOptionPane.QUESTION_MESSAGE);
-		recherche finder = new recherche();
-		finder.findFiles(nom);
-		JOptionPane.showMessageDialog(null, "Votre fichier " + nom, "est introuvable", JOptionPane.INFORMATION_MESSAGE);
-	}
+
+	
 }
---
+++ remarques.md
@@ -0,0 +1,12 @@
+* abscence total de commentaire / javadoc.
+* mauvaise structure du code;
+  * des classes dans le paquet "test" sont utilisés par l'application...
+  c'est un répertoire réservé aux tests...
+  vous ne pourrez pas builder/livrer ce code.
+  * le code est dupliqué 4 fois (!) dans des paquets au nom aussi
+  excplicite que `temporaire` et `temporaire2` 
+* Le dernier commit a pour message:
+
+    Version stable rendu final Interface + fonctions
+
+  Il manque la moitié des classes... cela représente bien votre maitrise du projet. 
